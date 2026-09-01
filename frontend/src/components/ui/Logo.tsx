5fb97c20f9d56c4eaf43a3950a2aeb0e88ef086d
Otieno <254analytics@gmail.com>
2026-09-01 13:02:09 +0300

    logo change

--- frontend/src/components/ui/Logo.tsx
+++ frontend/src/components/ui/Logo.tsx
@@ -12,7 +12,7 @@ export function Logo({ className = '', variant = 'full', dark = false }: LogoPro
       className={`overflow-hidden ${isMark ? 'h-10 w-10' : 'h-12 w-52'} ${dark ? 'rounded bg-white px-1' : ''} ${className}`}
     >
       <img
-        src="/brand/MSS_Logo.svg"
+        src="/brand/masterspace_logo.png"
         alt="Masterspace Solutions"
         className={isMark ? 'h-10 w-[120px] max-w-none object-left object-cover' : 'h-12 w-52 object-contain'}
       />
